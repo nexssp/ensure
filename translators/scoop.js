96a961bbb7347c43712976953257f9fe06eb892c
Marcin Polak <mapoart@gmail.com>
2021-06-01 00:03:51 +0200

    Added scoop installation

---
+++ translators/scoop.js
@@ -0,0 +1,12 @@
+const os = require('@nexssp/os/legacy')
+
+// first tag means all versions. We specified 1, because it does matter for the first tag
+const GentooFirst = os.getTags(os.distros.GENTOO, 1).first()
+const Windows = os.getTags(os.distros.WINDOWS, 1).first()
+const Windows10 = os.getTags(os.distros.WINDOWS, 10.0).second()
+const Windows100 = os.getTags().third()
+module.exports = {
+  [Windows]: {
+    install: `powershell -command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser" ; powershell -command "iex (new-object net.webclient).downloadstring('https://get.scoop.sh')"`,
+  },
+}
