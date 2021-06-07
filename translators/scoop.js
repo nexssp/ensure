bcada822298e78552a1dbbe9a62ef8131b2a72f7
Marcin Polak <mapoart@gmail.com>
2021-06-07 11:07:39 +0200

    Better scoop installation details if for any reason failed.

--- translators/scoop.js
+++ translators/scoop.js
@@ -7,7 +7,7 @@ const Windows10 = os.getTags(os.distros.WINDOWS, 10.0).second()
 const Windows100 = os.getTags().third()
 module.exports = {
   [Windows]: {
-    install: `powershell -command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser" && powershell -command "iex (new-object net.webclient).downloadstring('https://get.scoop.sh')"`,
+    install: `powershell -command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser" && powershell -command "iex (new-object net.webclient).downloadstring('https://get.scoop.sh')" && echo Please restart your terminal window.`,
   },
   linux: {
     install: `echo "Scoop is a package manager which works only for windows platforms."`,
