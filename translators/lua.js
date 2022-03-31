da8790a8a4c3634ab52f88128841717c9d8219f3
Marcin Polak <mapoart@gmail.com>
2022-03-31 19:26:02 +0200

    fixing workflow

--- translators/lua.js
+++ translators/lua.js
@@ -2,7 +2,9 @@ const os = require('@nexssp/os/legacy')
 
 // first tag means all versions. We specified 1, because it does matter for the first tag
 const DEBIAN = os.getTags(os.distros.DEBIAN, 1).first()
+const UBUNTU = os.getTags(os.distros.UBUNTU, 1).first()
 
 module.exports = {
   [DEBIAN]: 'lua5.3',
+  [UBUNTU]: 'lua5.3',
 }
