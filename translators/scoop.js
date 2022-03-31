527359fec61130a44c4a281e8ad5fa8a8525b969
Marcin Polak <mapoart@gmail.com>
2022-03-31 20:05:54 +0200

    fixing workflow

--- translators/scoop.js
+++ translators/scoop.js
@@ -8,9 +8,9 @@ const Windows10 = os.getTags(os.distros.WINDOWS, 10.0).second()
 const Windows100 = os.getTags().third()
 module.exports = {
   [Windows]: {
-    install: `Set-ExecutionPolicy RemoteSigned -scope CurrentUser ; Invoke-WebRequest -useb get.scoop.sh -outfile 'install.ps1' && .\\install.ps1 ${
+    install: `powershell -command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser" && powershell -command "Invoke-WebRequest -useb get.scoop.sh -outfile 'install.ps1' && .\\install.ps1 ${
       sudo ? '-RunAsAdmin' : ''
-    } && echo Please restart your terminal window.`,
+    }" && echo Please restart your terminal window.`,
     installLegacy: `powershell -command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser" && powershell -command "iex (new-object net.webclient).downloadstring('https://get.scoop.sh')" && echo Please restart your terminal window.`,
   },
   linux: {
