3b991e8d1f32f9fa105b136d493fe3e975908d72
Marcin Polak <mapoart@gmail.com>
2022-04-02 13:27:01 +0200

    fixing workflow

--- translators/scoop.js
+++ translators/scoop.js
@@ -1,8 +1,6 @@
 const os1 = require('@nexssp/os')
 const os = new os1()
-const sudo = os.sudo()
-
-console.log('SUDO!!!!!!!!!!', sudo)
+const isAdmin = os.isRoot()
 
 // first tag means all versions. We specified 1, because it does matter for the first tag
 const GentooFirst = os.getTags(os.distros.GENTOO, 1).first()
@@ -12,7 +10,7 @@ const Windows100 = os.getTags().third()
 module.exports = {
   [Windows]: {
     install: `"${os.getShell()}" -command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser" && "${os.getShell()}" -command "Invoke-WebRequest -useb get.scoop.sh -outfile 'install.ps1' ; .\\install.ps1 ${
-      sudo ? '-RunAsAdmin' : ''
+      isAdmin ? '-RunAsAdmin' : ''
     }" && echo You may need to restart terminal window.`,
     installLegacy: `powershell -command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser" && powershell -command "iex (new-object net.webclient).downloadstring('https://get.scoop.sh')" && echo Please restart your terminal window.`,
   },
